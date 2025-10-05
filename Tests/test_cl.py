b6cd1bbedace0b68bf46b5f0596856061b98f774
Dzelamonyuy0 <tumee@carleton.edu>
2025-10-05 16:36:50 -0500

    Update test_cl.py
    indentation corrections

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -5,12 +5,12 @@ from ProductionCode.drugshelperfuncs import *
 
 class SearchByState(unittest.Testcase):
     def setUp(self):
-    load_drugdata() #load data for each test case
+        load_drugdata() #load data for each test case
     def invalid_state_name(self):
     '''ensures that the function returns statement for invalid US state input'''
-    expected = "Not a US State"
-    result = search_by_state('Canada')
-    self.asssertEqual(result,expected)
+        expected = "Not a US State"
+        result = search_by_state('Canada')
+        self.asssertEqual(result,expected)
 
 class SearchByName(unittest.Testcase):
     def setUp(self):
