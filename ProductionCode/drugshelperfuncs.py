c9fa82f7aec538170eea4d694ac15092178f8d33
Ayobami Idris Bidokwu <bidokwua@carleton.edu>
2025-10-05 16:27:04 -0500

    Update drugshelperfuncs.py
    Added a statement for if the state name or year doesn't exist

--- ProductionCode/drugshelperfuncs.py
+++ ProductionCode/drugshelperfuncs.py
@@ -15,12 +15,16 @@ def search_by_state(state_name):
     '''returns results(row from dataset) based on state as input'''
     data = load_drugdata()
     results = [row for row in data if row['State'].strip().lower() == state_name.lower()]
+    if len(results) == 0:
+        print("No records found for state:", state_name)
     return results
 
 def search_by_year(year):
     ''''returns results (row from dataset) based on year as input'''
     data = load_drugdata()
     results = [row for row in data if row['Year'].strip() == str(year)]
+    if len(results) == 0:
+        print("No records found for year:", year)
     return results
 
     
