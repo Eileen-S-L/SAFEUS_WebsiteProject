21d675e839930b237ff23ee73a6cc2252edf94da
Dzelamonyuy0 <tumee@carleton.edu>
2025-10-05 18:27:40 -0500

    modified global data_file to locate the path of Minidrugdataset.csv and also changed local data_file to filing to avoid python confusion between both

--- ProductionCode/drugshelperfuncs.py
+++ ProductionCode/drugshelperfuncs.py
@@ -1,12 +1,14 @@
 import csv
 
-data_file = "Minidrugdataset.csv"
+import os
+data_file = os.path.join(os.path.dirname(__file__), "..", "Data", "Minidrugdataset.csv")
+
 
 def load_drugdata():
     '''loads the data and returns a list format'''
     data_list = []
-    with open (data_file, mode= 'r') as data_file:
-        csv_reader = csv.DictReader(data_file)
+    with open (data_file, mode= 'r') as filing:
+        csv_reader = csv.DictReader(filing)
         for row in csv_reader:
             data_list.append(row)
     return data_list
@@ -22,10 +24,13 @@ def search_by_state(state_name):
 def search_by_year(year):
     ''''returns results (row from dataset) based on year as input'''
     data = load_drugdata()
-    results = [row for row in data if row['Year'].strip() == str(year)]
+    results = [row for row in data_list if row['Year'].strip() == str(year)]
     if len(results) == 0:
         print("No records found for year:", year)
     return results
+
+
+print(load_drugdata())
 
     
     
