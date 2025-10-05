fd782e727ac73fe2dfa714bb398287c5392ed5bb
Dzelamonyuy0 <tumee@carleton.edu>
2025-10-05 16:09:51 -0500

    Update test_cl.py
    Created unittest class for search by State and its subfunction when invalid state is input

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -1,1 +1,13 @@
 #This is test_cl.py for team project -- commanline.py
+
+import unittest
+form ProductionCode.drugshelperfuncs import *
+
+class SearchByState(unittest.Testcase):
+  def setUp(self):
+    load_drugdata() #load data for each test case
+  def invalid_state_name(self):
+    '''ensures that the function returns statement for invalid US state input'''
+    expected = "Not a US State"
+    result = search_by_state('Canada')
+    self.asssertEqual(result,expected)
