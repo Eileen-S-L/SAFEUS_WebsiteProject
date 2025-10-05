bef70878717b0bdba857217570169fad1663f0e2
Dzelamonyuy0 <tumee@carleton.edu>
2025-10-05 15:39:27 -0500

    Update drugshelperfuncs.py
    Added a description for load_drugdata, search_by_state and search_by_year

--- ProductionCode/drugshelperfuncs.py
+++ ProductionCode/drugshelperfuncs.py
@@ -3,6 +3,7 @@ import csv
 data_file = "Minidrugdataset.csv"
 
 def load_drugdata():
+    '''loads the data and returns a list format'''
     data_list = []
     with open (data_file, mode= 'r') as data_file:
         csv_reader = csv.DictReader(file)
@@ -11,11 +12,13 @@ def load_drugdata():
     return data_list
             
 def search_by_state(state):
+    '''returns results(row from dataset) based on state as input'''
     data = load_drugdata()
     results = [row for row in data if row['State'].strip() == state.lower()]
     return results
 
 def search_by_year(year):
+    ''''returns results (row from dataset) based on year as input'''
     data = load_drugdata()
     results = [row for row in data if row['Year'].strip() == str(year)]
     return results
