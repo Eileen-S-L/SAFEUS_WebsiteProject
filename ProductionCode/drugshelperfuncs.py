34245e71565807c9244996c49ff8e2a26ddecb9f
Ayobami Idris Bidokwu <bidokwua@carleton.edu>
2025-10-05 15:12:36 -0500

    Update drugshelperfuncs.py
    This is the importation of data from the minidrug Rachel created and the state test

--- ProductionCode/drugshelperfuncs.py
+++ ProductionCode/drugshelperfuncs.py
@@ -1,8 +1,30 @@
-import pandas as pd
-#helper function for reading columns by the state name
-def searchstatename(state):
-    df = pd.read_csv("drugs-1.csv",
-                    dtype = {"State": str})
+# import pandas as pd
+# #helper function for reading columns by the state name
+# def searchstatename(state):
+#     df = pd.read_csv("drugs-1.csv",
+#                     dtype = {"State": str})
 
-#helper function for reading colums by the year
-def searchbyyear():
+# #helper function for reading colums by the year
+# def searchbyyear():
+
+imprort csv
+
+data_file = "minidrugs2.csv"
+
+def load_drugdata():
+    data_list = []
+    with open (data_file, mode= 'r') as data_file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data_list.append(row)
+    return data_list
+            
+def search_by_state(state):
+    data = load_drugdata()
+    results = [row for row in data if row['State'].strip().lower() == state_name.lower()]
+    if len(results) == 0:
+        print("No records found for state:", state_name)
+    return results
+
+def search_by_year(year):
+    
