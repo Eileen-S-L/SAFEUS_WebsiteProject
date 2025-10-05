f46dd8759c7c36b320afe4652f1d5ee8cdbbf26e
Rachel-Punter <punterr@carleton.edu>
2025-10-05 15:23:41 -0500

    import spelling

--- ProductionCode/drugshelperfuncs.py
+++ ProductionCode/drugshelperfuncs.py
@@ -1,13 +1,4 @@
-# import pandas as pd
-# #helper function for reading columns by the state name
-# def searchstatename(state):
-#     df = pd.read_csv("drugs-1.csv",
-#                     dtype = {"State": str})
-
-# #helper function for reading colums by the year
-# def searchbyyear():
-
-imprort csv
+import csv
 
 data_file = "minidrugs2.csv"
 
