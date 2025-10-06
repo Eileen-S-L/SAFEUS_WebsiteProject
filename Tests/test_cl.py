ad5262dfce95ab7d77f8a6911ae26d1a446627c5
Dzelamonyuy0 <tumee@carleton.edu>
2025-10-05 20:35:10 -0500

    debugged class searchByState and class SearchByName

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -3,20 +3,21 @@
 import unittest
 from ProductionCode.drugshelperfuncs import *
 
-class SearchByState(unittest.Testcase):
+class SearchByState(unittest.TestCase):
     def setUp(self):
         load_drugdata() #load data for each test case
-    def invalid_state_name(self):
-    '''ensures that the function returns statement for invalid US state input'''
-        expected = "Not a US State"
+    def test_invalid_state_name(self):
+        '''ensures that the function returns statement for invalid US state input'''
+        expected = "No records found for state Canada"
         result = search_by_state('Canada')
-        self.asssertEqual(result,expected)
+        self.assertEqual(result,expected)
 
-class SearchByName(unittest.Testcase):
+class SearchByName(unittest.TestCase):
     def setUp(self):
         load_drugdata()
-    def invalid_year(self):
+    def test_invalid_year(self):
         '''ensures that function returns appropriate statement for invalid year input'''
         expected = "No records found for year: 2100"
         result = search_by_year("2100")
         self.assertEqual(result, expected)
+
