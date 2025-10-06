6512846548a01b3e9dec588074899be3ad0ad920
Dzelamonyuy0 <tumee@carleton.edu>
2025-10-05 21:29:21 -0500

    debug

--- ProductionCode/drugshelperfuncs.py
+++ ProductionCode/drugshelperfuncs.py
@@ -1,14 +1,13 @@
 import csv
 
-import os
-data_file = os.path.join(os.path.dirname(__file__), "..", "Data", "Minidrugdataset.csv")
 
+data_file = '../Data/Minidrugdataset.csv' 
 
 def load_drugdata():
     '''loads the data and returns a list format'''
     data_list = []
-    with open (data_file, mode= 'r') as filing:
-        csv_reader = csv.DictReader(filing)
+    with open (data_file, mode= 'r') as file:
+        csv_reader = csv.DictReader(file)
         for row in csv_reader:
             data_list.append(row)
     return data_list
@@ -18,9 +17,9 @@ def search_by_state(state_name):
     data = load_drugdata()
     results = []
     for row in data:
-        if row['\ufeffState'].strip().lower() == state_name.lower():
+        if row['State'].strip().lower() == state_name.lower():
             results.append(row)
-    if results == []:
+    if len(results) == 0:
         return "No records found for state " + state_name
     return results
 
@@ -29,7 +28,7 @@ def search_by_year(year):
     data = load_drugdata()
     results = []
     for row in data:
-        if row['Year'].lower()==str(year):
+        if row['Year']==str(year).strip():
             results.append(row)
     if results == []:
         return "No records found for year: " + str(year)
