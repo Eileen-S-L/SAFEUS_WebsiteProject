816ee4ac05b504a0817c3d59ffe53da4f56cfc73
Rachel-Punter <punterr@carleton.edu>
2025-10-06 12:24:31 -0500

    idk

--- command_line.py
+++ command_line.py
@@ -14,25 +14,26 @@ def main ():
     parser.add_argument('--year')
     parser.add_argument('--state')
     args= parser.parse_args()
+    usage = 'python3 command_line.py --year/state "year"/"state" '
 
-    if (sys.argv[0]=="--year"):
+    if (args[0]=="--year"):
         #function of our team project that nees us to write up from scratch
         #the function could really name as anything, I named it like this here
         # get_drugdata_year(): grabs rows of related data based on the year enters"
         if (args.size()==1 or args.size()>2):
             print(usage)
         else:
-            search_by_year(sys.argv[1])
+            search_by_year(args[1])
 
-    if (sys.argv[0]=="--state"):
+    if (args[0]=="--state"):
         if (args.size()==1 or args.size()>2):
             print(usage)
         else:
             search_by_state(args[1])
-            return search_by_state(sys.argv[1])
+            return search_by_state(args[1])
 
-    # if (sys.argv[0]!="--state" or sys.arg[0]!="--year"):
-    #     print(usage)
+    if (args[0]!="--state" or args[0]!="--year"):
+        print(usage)
     # else:
     #     print(usage)
 
