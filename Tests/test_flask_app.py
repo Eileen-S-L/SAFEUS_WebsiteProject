6c6f9c7f7a085468afa2e0d8c40f1232f7d0f3bd
Rachel-Punter <punterr@carleton.edu>
2025-10-09 16:24:43 -0500

    team

---
+++ Tests/test_flask_app.py
@@ -0,0 +1,38 @@
+from flask_app import *
+import unittest
+
+class TestSOMEPAGE(unittest.TestCase):
+    def test_route(self):
+        self.app = app.test_client()
+        response = self.app.get('/', follow_redirects=True)
+        """ Arguments: function and expected output
+        Return Value: None
+        Purpose: For making sure that the proper output is shown on the homepage"""
+        self.assertEqual(b"Welcome to the homepage! <br> <br> How to view the data: <br> Go to route '/' and put in a row/column to see data. Column 0 shows state names, column 1 shows year, and column 3 shows population size for ages 12-17 <br> Type in route '/year/*a year*' to view the data according to year", response.data)
+
+class TestAboutPage(unittest.TestCase):
+    def test_route(self):
+        self.app = app.test_client()
+        response = self.app.get('/about', follow_redirects=True)
+        """ Arguments: function and expected output
+        Return Value: None
+        Purpose: For making sure that the proper output is shown on the about page"""
+        self.assertEqual(b'Hello, this is the about page', response.data)
+
+class TestYearDisplay(unittest.TestCase):
+    def test_routeStandard(self):
+        output = b"[{'\ufeffState': 'Alabama', 'Year': '2002', 'Population.12-17': '380805'}, {'\ufeffState': 'Alaska', 'Year': '2002', 'Population.12-17': '69400'}, {'\ufeffState': 'Arizona', 'Year': '2002', 'Population.12-17': '485521'}, {'\ufeffState': 'Arkansas', 'Year': '2002', 'Population.12-17': '232986'}, {'\ufeffState': 'California', 'Year': '2002', 'Population.12-17': '3140739'}, {'\ufeffState': 'Colorado', 'Year': '2002', 'Population.12-17': '385648'}, {'\ufeffState': 'Connecticut', 'Year': '2002', 'Population.12-17': '295157'}, {'\ufeffState': 'Delaware', 'Year': '2002', 'Population.12-17': '66477'}, {'\ufeffState': 'District of Columbia', 'Year': '2002', 'Population.12-17': '33192'}, {'\ufeffState': 'Florida', 'Year': '2002', 'Population.12-17': '1346297'}, {'\ufeffState': 'Georgia', 'Year': '2002', 'Population.12-17': '748467'}, {'\ufeffState': 'Hawaii', 'Year': '2002', 'Population.12-17': '103803'}, {'\ufeffState': 'Idaho', 'Year': '2002', 'Population.12-17': '128028'}, {'\ufeffState': 'Illinois', 'Year': '2002', 'Population.12-17': '1082396'}, {'\ufeffState': 'Indiana', 'Year': '2002', 'Population.12-17': '541577'}, {'\ufeffState': 'Iowa', 'Year': '2002', 'Population.12-17': '246347'}, {'\ufeffState': 'Kansas', 'Year': '2002', 'Population.12-17': '241178'}, {'\ufeffState': 'Kentucky', 'Year': '2002', 'Population.12-17': '327727'}, {'\ufeffState': 'Louisiana', 'Year': '2002', 'Population.12-17': '406965'}, {'\ufeffState': 'Maine', 'Year': '2002', 'Population.12-17': '108861'}, {'\ufeffState': 'Maryland', 'Year': '2002', 'Population.12-17': '476696'}, {'\ufeffState': 'Massachusetts', 'Year': '2002', 'Population.12-17': '508325'}, {'\ufeffState': 'Michigan', 'Year': '2002', 'Population.12-17': '895753'}, {'\ufeffState': 'Minnesota', 'Year': '2002', 'Population.12-17': '446545'}, {'\ufeffState': 'Mississippi', 'Year': '2002', 'Population.12-17': '257508'}, {'\ufeffState': 'Missouri', 'Year': '2002', 'Population.12-17': '491394'}, {'\ufeffState': 'Montana', 'Year': '2002', 'Population.12-17': '81697'}, {'\ufeffState': 'Nebraska', 'Year': '2002', 'Population.12-17': '152465'}, {'\ufeffState': 'Nevada', 'Year': '2002', 'Population.12-17': '184670'}, {'\ufeffState': 'New Hampshire', 'Year': '2002', 'Population.12-17': '113457'}, {'\ufeffState': 'New Jersey', 'Year': '2002', 'Population.12-17': '719658'}, {'\ufeffState': 'New Mexico', 'Year': '2002', 'Population.12-17': '176611'}, {'\ufeffState': 'New York', 'Year': '2002', 'Population.12-17': '1562426'}, {'\ufeffState': 'North Carolina', 'Year': '2002', 'Population.12-17': '685632'}, {'\ufeffState': 'North Dakota', 'Year': '2002', 'Population.12-17': '54387'}, {'\ufeffState': 'Ohio', 'Year': '2002', 'Population.12-17': '987986'}, {'\ufeffState': 'Oklahoma', 'Year': '2002', 'Population.12-17': '302673'}, {'\ufeffState': 'Oregon', 'Year': '2002', 'Population.12-17': '297076'}, {'\ufeffState': 'Pennsylvania', 'Year': '2002', 'Population.12-17': '1028108'}, {'\ufeffState': 'Rhode Island', 'Year': '2002', 'Population.12-17': '85295'}, {'\ufeffState': 'South Carolina', 'Year': '2002', 'Population.12-17': '345629'}, {'\ufeffState': 'South Dakota', 'Year': '2002', 'Population.12-17': '69742'}, {'\ufeffState': 'Tennessee', 'Year': '2002', 'Population.12-17': '473558'}, {'\ufeffState': 'Texas', 'Year': '2002', 'Population.12-17': '2018953'}, {'\ufeffState': 'Utah', 'Year': '2002', 'Population.12-17': '229447'}, {'\ufeffState': 'Vermont', 'Year': '2002', 'Population.12-17': '53924'}, {'\ufeffState': 'Virginia', 'Year': '2002', 'Population.12-17': '607438'}, {'\ufeffState': 'Washington', 'Year': '2002', 'Population.12-17': '528622'}, {'\ufeffState': 'West Virginia', 'Year': '2002', 'Population.12-17': '139163'}, {'\ufeffState': 'Wisconsin', 'Year': '2002', 'Population.12-17': '482686'}, {'\ufeffState': 'Wyoming', 'Year': '2002', 'Population.12-17': '45377'}]"
+        self.app = app.test_client()
+        response = self.app.get('/year/2002', follow_redirects=True)
+        """ Arguments: function and expected output
+        Return Value: None
+        Purpose: Ensuring that the standard case for the year display route passes"""
+        self.assertIn(output, response.data)
+    
+    def test_routeEdge(self):
+        self.app = app.test_client()
+        response = self.app.get('/year/1990', follow_redirects=True)
+        """ Arguments: function and expected output
+        Return Value: None
+        Purpose: Makes sure that the edge case for the year display route passes"""
+        self.assertIn(b'No records found for year: 1990', response.data)
--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,47 @@
 '''
 The eventual location for the Flask app interface for the project.
 '''
+from flask import Flask
+import csv
+from ProductionCode.drugshelperfuncs import *
+
+app = Flask(__name__)
+
+""" Arguments: route
+    Return Value: String/explaination of the website
+    Purpose: To explain to users how to use routes to view data"""
+@app.route('/')
+def homepage():
+    return "Welcome to the homepage! <br> <br> How to view the data: <br> Go to route '/' and put in a row/column to see data. Column 0 shows state names, column 1 shows year, and column 3 shows population size for ages 12-17 <br> Type in route '/year/*a year*' to view the data according to year"
+
+""" Arguments: None
+    Return Value: None
+    Purpose: Loading the csv data into the code so it can be accessed"""
+dummy_data = []
+def load_data():
+    with open('Data/Minidrugdataset.csv', newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            dummy_data.append(row)
+
+""" Arguments: route and how to view the slashes in the route
+    Return Value: The data by row and column number
+    Purpose: To allow users to search for data by the colum and row number"""
+@app.route('/<row>/<column>', strict_slashes = False)
+def get_cell(row, column):
+    return dummy_data[int(row)][int(column)]
+
+""" Arguments: route and how to view the slashes
+    Return Value: All the data from rows with the inputted year
+    Purpose: To allow users to see drug data by year"""
+@app.route('/year/<year>', strict_slashes = False)
+def displaydatabyyear(year):
+    return str(search_by_year(year))
+
+# @app.route('/state/<state>', strict_slashes = False)
+# def displaydatabystate(state):
+#     return str(search_by_state(state)
+
+if __name__ == '__main__':
+    load_data()
+    app.run()
