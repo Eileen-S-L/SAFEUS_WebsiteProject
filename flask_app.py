0aa30f3b4621b9d2407aef1a6fccff4af0937605
Rachel-Punter <punterr@carleton.edu>
2025-10-24 11:27:13 -0500

    b5

--- flask_app.py
+++ flask_app.py
@@ -3,51 +3,39 @@ The eventual location for the Flask app interface for the project.
 '''
 from flask import Flask
 import csv
-from ProductionCode.drugshelperfuncs import *
+from ProductionCode.datasource import *
 # from ProductionCode.datasource import DataSource
+
+data = DataSource()
 
 app = Flask(__name__)
 """ Arguments: route
     Return Value: String/explaination of the website
     Purpose: To explain to users how to use routes to view data"""
 
-data_list = load_drugdata()
 @app.route('/')
 def homepage():
     return "Welcome to the homepage! <br> <br> How to view the data: <br> Go to route '/' and put in a row/column to see data. Column 0 shows state names, column 1 shows year, and column 3 shows population size for ages 12-17 <br> Type in route '/year/*a year*' to view the data according to year"
 
-""" Arguments: None
-    Return Value: None
-    Purpose: Loading the csv data into the code so it can be accessed"""
-# dummy_data = []
-# def load_data():
-#     with open('Data/Minidrugdataset.csv', newline='') as f:
-#         reader = csv.reader(f)
-#         for row in reader:
-#             dummy_data.append(row)
 
-""" Arguments: route and how to view the slashes in the route
-    Return Value: The data by row and column number
-    Purpose: To allow users to search for data by the colum and row number"""
-# @app.route('/<row>/<column>', strict_slashes = False)
-# def get_cell(row, column):
-#     return data_list[int(row)][int(column)]
 
 """ Arguments: route and how to view the slashes
     Return Value: All the data from rows with the inputted year
     Purpose: To allow users to see drug data by year"""
-@app.route('/year/<year>', strict_slashes = False)
-@app.route('/Year/<year>', strict_slashes = False)
-def displaydatabyyear(year):
+@app.route('substance/<substance>/year/<year>', strict_slashes = False)
+@app.route('Substance/<substance>/Year/<year>', strict_slashes = False)
+def displaydatabyyear(substance, year):
     year = str(year).strip()
-    return str(search_by_year(year))
+    substance = str(substance).strip()
+    return str(data.get_data_by_year(substance, year))
 
 
-@app.route('/state/<state>', strict_slashes = False)
-@app.route('/State/<state>', strict_slashes = False)
-def displaydatabystate(state):
+@app.route('substance/<substance>/state/<state>', strict_slashes = False)
+@app.route('Substance/<substance>/State/<state>', strict_slashes = False)
+def displaydatabystate(substance, state):
     state = str(state).strip()
-    return str(search_by_state(str(state)))
+    substance = str(substance).strip()
+    return str(data.get_data_by_state(substance, state))
 
 """ Arguments: e
     Return Value: Instructions on how to get to an actual page
