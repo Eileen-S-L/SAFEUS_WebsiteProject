e867b6ebcf1c6c30933af2a19b815381b8a02d21
Rachel-Punter <punterr@carleton.edu>
2025-11-05 11:33:30 -0600

    d

--- flask_app.py
+++ flask_app.py
@@ -43,7 +43,8 @@ def displaydatabyyear(substance, year):
 def displaydatabystate(substance, state):
     state = str(state).strip()
     substance = str(substance).strip()
-    return render_template("displaydata.html", state, substance, display = str(data.get_data_by_state(substance, state)))
+    display = str(data.get_data_by_state(substance, state))
+    return render_template("displaydata.html", state, substance, display)
     #return str(data.get_data_by_state(substance, state))
 
 """ Arguments: route
