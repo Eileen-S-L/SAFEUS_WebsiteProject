aa37627713d9352c29f61957dac7a7239ad1a583
Dzelamonyuy0 <tumee@carleton.edu>
2025-11-06 16:01:18 -0600

    modified get_data_by_year function

--- ProductionCode/datasource.py
+++ ProductionCode/datasource.py
@@ -28,6 +28,8 @@ class DataSource:
         '''Argument: Year & substance
         Return: records, which is a list of data related to searched-year
         Purpose: Select wanted data from connected database'''
+        if not isinstance(Year, int):
+            print "Year input must be valid year"
         if (int(Year)>2018 or int(Year)<2002):
             return "We only have data from 2002 to 2018. Please input one of these years :)"
         try:
