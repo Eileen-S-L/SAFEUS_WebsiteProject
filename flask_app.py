fbc45fc573c5aa4516dd5b79497bcb7d08e4b2dc
bidokwua <bidokwua@carleton.edu>
2025-11-14 15:29:46 -0600

    port

--- flask_app.py
+++ flask_app.py
@@ -117,4 +117,4 @@ def wrongfunction(random):
     raise Exception("Intentional error for testing 500 handler")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5206)
+    app.run(host='0.0.0.0', port=5221)
