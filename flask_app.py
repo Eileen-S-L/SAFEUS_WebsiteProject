7b992b77dd17013b5d59d1e17cfccfdbce368de6
bidokwua <bidokwua@carleton.edu>
2025-11-22 04:16:51 -0600

    html code cleaning

--- flask_app.py
+++ flask_app.py
@@ -16,18 +16,18 @@ app = Flask(__name__)
     Purpose: To explain to users how to use routes to view data"""
 @app.route('/')
 def homepage():
-    return render_template("homepage.html")
+    return render_template("index.html")
 
 @app.route('/start_explore', strict_slashes=False)
 def Explorepage():
-    return render_template("start_explorepage.html")
+    return render_template("start_explore.html")
 
 """ Arguments: route '/about'
     Return Value: A website page explaining the data
     Purpose: To allow users to learn about the origins of the data"""
 @app.route('/about', strict_slashes = False)
 def aboutthedata():
-    return render_template("aboutdatapage.html")
+    return render_template("about.html")
 
 """ Arguments: route
     Return Value: the data by substance and year
@@ -37,7 +37,7 @@ def displaydatabyyear(year, substance):
     year = str(year)
     substance = str(substance).strip()
     search_result=data.get_data_by_year(substance, year)
-    Html_template='unifieddata.html'
+    Html_template='data_results.html'
     return render_template(Html_template, result=search_result, type = year, sub = substance, subname = substance.title())
 
 """ Arguments: route
@@ -48,7 +48,7 @@ def displaydatabystate(state, substance):
     state = str(state).strip()
     substance = str(substance).strip()
     search_result=data.get_data_by_state(substance, state)
-    Html_template='unifieddata.html'
+    Html_template='data_results.html'
     return render_template(Html_template, result=search_result, type = state.title(), sub = substance, subname = substance.title())
 
 
@@ -57,28 +57,28 @@ def displaydatabystate(state, substance):
     Purpose: To give users a place to search for data by year"""
 @app.route('/year', strict_slashes = False)
 def search_year_online():
-    return render_template("yeardatapage.html", yeardisplay='Seach By Year')
+    return render_template("search_year.html", yeardisplay='Seach By Year')
 
 """ Arguments: route
     Return Value: a search box for state
     Purpose: To give users a place to search for data by state"""
 @app.route('/state', strict_slashes = False)
 def search_state_online():
-    return render_template("statedatapage.html", statedisplay='Search By State')
+    return render_template("search_state.html", statedisplay='Search By State')
 
 """ Arguments: e
     Return Value: Instructions on how to get to an actual page
     Purpose: Tell users that they went to the wrong/nonexistent page and tell them how to get to a correct/real one"""
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404errorpage.html", title = "404 Not Found")
+    return render_template("404.html", title = "404 Not Found")
 
 """ Arguments: e
     Return Value: A message stating that there is an error
     Purpose: To catch an error within the python code"""
 @app.errorhandler(500)
 def python_bug(e):
-    return render_template("500errorpage.html", title = "500 Error")
+    return render_template("500.html", title = "500 Error")
 
 
 """ Arguments: random word
